8d3116860829b3e7575048bf66df42293295c13c
Jeffrey Martinez <martinezjf2@gmail.com>
2020-02-03 23:20:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -39,6 +39,7 @@ class Scraper
     course = Course.new
     course.title = post.css("h2").text
     course.schedule = post.css(".date").text
+    course.description = post.css("p").text
   end
 end
 
