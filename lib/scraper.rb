6deb20882ead6bbc79b37d89005aae568a3057c4
Jeffrey Martinez <martinezjf2@gmail.com>
2020-02-03 23:19:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,6 +34,13 @@ class Scraper
     self.get_page.css(".post")
   end
 
+  def make_courses
+    self.get_courses.each do |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+  end
+
 end
 
 Scraper.new.get_page
